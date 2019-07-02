2c25baa2edadfc7de3790d8d14c8495fa2e97937
renkill <52439086+renkill@users.noreply.github.com>
2019-07-02 02:56:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
-codigo fonte da sua deface !!!
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hacked By Rekill</title>
+	<link href="https://fonts.googleapis.com/css?family=Fjalla+One|Pacifico|Passion+One&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	
+	<style type="text/css">
+	h1 {color: red ; font-family: 'Fjalla One', sans-serif; text-align: center; }	
+	body {background: black}
+	p {color: red ; font-family: 'Fjalla One', sans-serif; text-align: center;font-size: 29px}
+	p.as {color: white}
+
+
+	</style>
+
+
+</head>
+      
+<body>
+      <embed src="https://instaud.io/_/3SDf.mp3" width="0" height="0">
+
+<h1>Pwned por Renkill</h1>
+<p align="center" class="as"><img src="
+https://3.bp.blogspot.com/-6VmyL5f-6xk/W_uQsuHTyLI/AAAAAAABrk8/YskARyj0GYUalGWfuiekRBF17gAKvla_QCK4BGAYYCw/s640/qjEARp.gif" border="1">
+<p>Quem nao faz quer falar de quem fez</p>
+<hr>
+<p><marquee>Greatz:Demonsad,Nedlock,Kami,TheFox,zClient</marquee></p>
+
+
+</html>
